580523dc5276fa894301cdafb56f6768e3733b2d
Felix Leblanc <felix.leblanc1305@gmail.com>
2020-02-24 15:42:56 -0500

    Fix: Remove inputs of outputs

--- extract_wallet_explorer.py
+++ extract_wallet_explorer.py
@@ -48,6 +48,9 @@ def get_node_from_address(address):
     tx_csv_url = soup.find('a', string="Download as CSV")['href']
     return re.match('/wallet/(\w+)\?format=csv', tx_csv_url).group(1)
 
+def extract_addresses(rows):
+    return rows.str.extractall('(\w{16})')[0].tolist()
+
 def transactions_from_node_id(node_id, current_hop, max_hops, tx_type=None):
     logger.debug(f'crawling node {node_id} with hop {current_hop}')
     url = f'{base_url}/wallet/{node_id}?format=csv'
@@ -74,10 +77,15 @@ def transactions_from_node_id(node_id, current_hop, max_hops, tx_type=None):
             new_nodes = transactions['sent to']
         else:
             new_nodes = transactions['received from'].append(transactions['sent to'])
-        new_nodes = new_nodes.str.extractall('(\w{16})')[0].tolist()
+        new_nodes = extract_addresses(new_nodes)
 
-        tx_list = [transactions_from_node_id(x, new_hop, max_hops, tx_type) for x in new_nodes]
-        new_transactions = pd.concat(tx_list)
+        if new_hop <= 2:
+            inputs = [transactions_from_node_id(x, new_hop, max_hops, 'in') for x in extract_addresses(transactions['received from'])]
+            outputs = [transactions_from_node_id(x, new_hop, max_hops, 'out') for x in extract_addresses(transactions['sent to'])]
+            new_transactions = pd.concat(inputs).append(pd.concat(outputs))
+        else:
+            tx_list = [transactions_from_node_id(x, new_hop, max_hops, tx_type) for x in new_nodes]
+            new_transactions = pd.concat(tx_list)
     
     # Replace na values by the current node_id
     transactions['received from'].fillna(node_id, inplace=True)
@@ -88,23 +96,35 @@ def transactions_from_node_id(node_id, current_hop, max_hops, tx_type=None):
     
     return transactions.append(new_transactions)
 
+def map_addresses_to_id(row, labels):
+    row['Source'] = labels.index(row['Source'])
+    row['Target'] = labels.index(row['Target'])
+
+    return row
+
 def crawl(address, max_hops, tx_type=None):
     logger.debug(f'crawling address {address}')
     node_id = get_node_from_address(address)
     transactions = transactions_from_node_id(node_id, 1, max_hops, tx_type)
+    transactions = transactions.drop_duplicates(subset=['received from', 'sent to'])
     transactions['price_usd'] = transactions['date'].str.split(' ').str[0].apply(lambda x: conversion_table[x])
     transactions['sum_usd'] = np.where(transactions['received amount'].isna(), transactions['sent amount'] * transactions['price_usd'], transactions['received amount'] * transactions['price_usd'])
+
+    # Write nodes
+    transactions['other_node'] = np.where(transactions['received from'] == transactions['node_id'], transactions['sent to'], transactions['received from'])
+    col_map = {'other_node': 'Label', 'hop': 'hops'}
+    final_nodes = transactions[col_map.keys()].rename(columns=col_map)
+    node_row = pd.DataFrame({'Label': node_id, 'hops': 0}, index=[0])
+    final_nodes = pd.concat([node_row, final_nodes])
+    final_nodes['Id'] = range(len(final_nodes))
+    final_nodes[['Id', 'Label', 'hops']].to_csv(f'{address}({node_id})_nodes.csv', index=False)
 
     # Write transactions
     col_map = {'received from': 'Source', 'sent to': 'Target', 'sum_usd': 'Weight'}
     final_txs = transactions[col_map.keys()].rename(columns=col_map)
-    final_txs.to_csv(f'{address}({node_id})_ties.csv', index=False)
-
-    # Write nodes
-    transactions['other_node'] = np.where(transactions['received from'] == transactions['node_id'], transactions['sent to'], transactions['received from'])
-    col_map = {'node_id': 'Id', 'other_node': 'Label', 'hop': 'hops'}
-    final_nodes = transactions[col_map.keys()].rename(columns=col_map)
-    final_nodes.to_csv(f'{address}({node_id})_nodes.csv', index=False)
+    final_txs['Type'] = 'Directed'
+    final_txs = final_txs.apply(lambda x: map_addresses_to_id(x, final_nodes['Label'].tolist()), axis=1)
+    final_txs[['Source', 'Target', 'Type', 'Weight']].to_csv(f'{address}({node_id})_ties.csv', index=False)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--address", help="Specify an address to crawl on wallet explorer")
